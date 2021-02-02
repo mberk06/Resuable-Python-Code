fd8bd724fe57f91ba3110782c5e011945758be31
Michael Berk <michaelberk@Michaels-MacBook-Pro.local>
2021-02-02 18:29:43 -0500

    Survival analysis draft

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Resuable-Python-Code
 Python code templates for projects that I have worked on.
+
+### Files
+survival_analysis.py: survival analysis is a statistical method to assess the probability that an event will happen within a certain time period.  
+- [Towards Data Science Tutorial](https://towardsdatascience.com/survival-analysis-intuition-implementation-in-python-504fde4fcf8e)
+
---
+++ survival_analysis.py
@@ -0,0 +1,18 @@
+"""
+Description: survival analysis template in python 
+Author: Michael Berk
+Date: Winter 2021
+
+"""
+
+# Kaplan Meier: univariate non-parametric model that is good at developing a baseline fit
+from lifelines import KaplanMeierFitter
+
+time_to_event = [9,2,7,4,3,7,2]
+event = [1,0,1,1,0,0,0]
+
+kmf = KaplanMeierFitter()
+kmf.fit(time_to_event, event)
+
+kmf.plot(ci_show=False)
+
